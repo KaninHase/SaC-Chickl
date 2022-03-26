3354cbb692456619da2f18984c765a52aa3019e8
KaninHase <97570748+KaninHase@users.noreply.github.com>
2022-03-25 18:15:19 -0600

    Add files via upload

---
+++ sac-chick.js
@@ -0,0 +1,50 @@
+
+function softChick() {
+ document.getElementById("🌞").bgcolor  = "#fed";
+ document.getElementById("p").innerHTML = "Buen Día (^▽^)";
+ document.getElementById("weichesChick").src='AS0005044_08.gif';
+ document.body.style.background = "#fe9";
+ document.getElementById("wrapper").style.color = "#eb5"; 
+ document.getElementById("footer").style.color = "#eb5";
+ document.getElementById("button").style.background="#eb5";
+}
+
+function freundlichesChick() {
+ document.getElementById("🐥").bgcolor  = "#fed";
+ document.getElementById("p").innerHTML = "Hoy será un lindo día ◝(ᵔᵕᵔ)◜";
+ document.getElementById("weichesChick").src='3224080a6f071f991616f94e84ecedc3756a975ar1-370-210_hq.gif';
+ document.body.style.background = "#aef";
+ document.getElementById("wrapper").style.color = "#f08"; 
+ document.getElementById("footer").style.color = "#f08";
+ document.getElementById("button").style.background="#f08";
+}
+
+function cuteChick() {
+ document.getElementById("🍨").bgcolor  = "#3c3";
+ document.getElementById("p").innerHTML = "Hoy será un lindo día ◝(ᵔᵕᵔ)◜";
+ document.getElementById("weichesChick").src='422001185.png';
+ document.body.style.background = "#3c3";
+ document.getElementById("wrapper").style.color = "#bf0"; 
+ document.getElementById("footer").style.color = "#bf0";
+ document.getElementById("button").style.background="#bf0";
+}
+
+function penguinAndChick(){
+ document.getElementById("🐧").bgcolor  = "#c9a";
+ document.getElementById("p").innerHTML = "Dulces Sueños  (。-ω-)zzz";
+ document.getElementById("weichesChick").src='AW3914353_10.gif';
+ document.body.style.background = "#c9a";
+ document.getElementById("wrapper").style.color = "#ebc"; 
+ document.getElementById("footer").style.color = "#ebc";
+ document.getElementById("button").style.background="#ebc";
+}
+
+function sleepingChick(){
+ document.getElementById("🌜").bgcolor  = "#117";
+ document.getElementById("p").innerHTML = "Dulces Sueños  (。-ω-)zzz";
+ document.getElementById("weichesChick").src='AS0005044_09.gif';
+ document.body.style.background = "#117";
+ document.getElementById("wrapper").style.color = "#fed"; 
+ document.getElementById("footer").style.color = "#fed";
+ document.getElementById("button").style.background="#fed";
+}
